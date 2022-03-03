1f8419d4be8a5da95208c8eddecd3ba6af491d57
imran <imran.mohammed@searce.com>
2022-03-03 12:22:55 +0530

    made changes in dockerfile

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import os 
+
+username = os.getenv('USER_NAME')
+app = Flask(__name__)
+
+#route.decorator()
+@app.route("/")
+def home_page():
+    return "Welcome to the searce"
+
+@app.route("/Health")
+def status():
+    return "I am up"
+
+@app.route("/user_name")
+def display_name():
+    return "{}!".format(username)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
